f5e57820ed95fda746bf1858f8ab0bc5b0530dd5
William Soukkachang <Wsoukkachang@yahoo.com>
2020-07-17 01:20:04 -0500

    Path to root script.

---
+++ path_to_root.py
@@ -0,0 +1,63 @@
+from queue import Queue
+
+def convert_arr_to_binary_tree(arr):
+    """
+    Takes arr representing level-order traversal of Binary Tree 
+    """
+    index = 0
+    length = len(arr)
+    
+    if length <= 0 or arr[0] == -1:
+        return None
+
+    root = BinaryTreeNode(arr[index])
+    index += 1
+    queue = Queue()
+    queue.put(root)
+    
+    while not queue.empty():
+        current_node = queue.get()
+        left_child = arr[index]
+        index += 1
+        
+        if left_child is not None:
+            left_node = BinaryTreeNode(left_child)
+            current_node.left = left_node
+            queue.put(left_node)
+        
+        right_child = arr[index]
+        index += 1
+        
+        if right_child is not None:
+            right_node = BinaryTreeNode(right_child)
+            current_node.right = right_node
+            queue.put(right_node)
+    return root
+
+def path_from_root_to_node(root, data):
+    """
+    Assuming data as input to find the node
+    The solution can be easily changed to find a node instead of data
+    :param data:
+    :return:
+    """
+    output = path_from_node_to_root(root, data)
+    return list(reversed(output))
+
+def path_from_node_to_root(root, data):
+    if root is None:
+        return None
+
+    elif root.data == data:
+        return [data]
+
+    left_answer = path_from_node_to_root(root.left, data)
+    if left_answer is not None:
+        left_answer.append(root.data)
+        return left_answer
+
+    right_answer = path_from_node_to_root(root.right, data)
+    if right_answer is not None:
+        right_answer.append(root.data)
+        return right_answer
+    return None
