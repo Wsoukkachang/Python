931b89d5ce5d4e07654403c3baa63ef0f2dae8b4
William Soukkachang <Wsoukkachang@yahoo.com>
2020-10-27 20:06:33 -0500

    Used bind() to have access to local var.

--- js_sandbox.js
+++ js_sandbox.js
@@ -6,3 +6,14 @@ function getMaxNumber(arr){
 }
  
 getMaxNumber(array) // should return 4
+
+const character = {
+  name: 'Simon',
+  getCharacter() {
+    return this.name;
+  }
+};
+const giveMeTheCharacterNOW = character.getCharacter.bind(character);
+ 
+//How Would you fix this?
+console.log('?', giveMeTheCharacterNOW()); //this should return 'Simon' but doesn't
